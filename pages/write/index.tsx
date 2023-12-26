b944a80a2241726b3ca8046f6bb784c8bddaa2a2
hyeongjin <kimjin5537@daum.net>
2023-12-26 15:35:09 +0900

    MOCO-14 게시글 작성 페이지 route 처리

---
+++ pages/write/index.tsx
@@ -0,0 +1,5 @@
+import {AppLayout} from '@/layouts/AppLayout';
+
+export default function Write() {
+  return <AppLayout>게시글 작성 페이지</AppLayout>;
+}
--- routes/index.ts
+++ routes/index.ts
@@ -1,2 +1,5 @@
 // 회원가입 페이지
 export const ROUTE_SIGNUP = '/signup';
+
+// 새 글 작성 페이지
+export const ROUTE_WRITE = '/write';
