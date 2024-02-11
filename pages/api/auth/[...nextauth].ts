c8d79b2c737cd366ba3a97458f7e73edf6df0449
Hyeongjin Kim <kimjin5537@daum.net>
2024-02-12 01:44:44 +0900

    .

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -30,7 +30,6 @@ export default NextAuth({
             accessToken: account.access_token,
           })
           .then(res => res.data);
-        console.log('test##', process.env.NEXT_PUBLIC_MOCO_API_URL);
         if (data?.accessToken) {
           token.isLogin = true;
           token.accessToken = data.accessToken;
