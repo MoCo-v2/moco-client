26dc7aec034b6e603dea6643d96ffc5d1a4db83d
hyeongjin <kimjin5537@daum.net>
2024-02-13 14:43:48 +0900

    MOCO-67 사용자 프로필 팝업 디자인 변경

--- containers/ProfileDetailModal/index.tsx
+++ containers/ProfileDetailModal/index.tsx
@@ -1,9 +1,8 @@
-import {Modal} from '@/components';
-
 import {useUserProfile} from '@/hooks/useUserProfile';
+
 import {POSITIONS, STACKS} from '@/consts';
 
-import {Wrppaepr} from './style';
+import {CustomModal, CustomBody} from './style';
 
 interface Props {
   userId?: string;
@@ -18,32 +17,34 @@ export const ProfileDetailModal = (props: Props) => {
   if (!user) return null;
 
   return (
-    <Modal show={!!userId} onHide={() => setUserId(undefined)}>
-      <Wrppaepr>
+    <CustomModal show={!!userId} onHide={() => setUserId(undefined)} centered>
+      <CustomBody>
         <div className="top">
-          <img src={user.picture} alt="profile" draggable={false} />
-          <div className="user-info">
-            <div className="user-info-top">
-              <span className="name">{user.name}</span>
-            </div>
-            <div className="user-info-middle">
+          <div className="top-left">
+            <img src={user.picture} alt="profile" draggable={false} />
+          </div>
+          <div className="top-right">
+            <div className="info">
               <span className="position">
                 {POSITIONS.find(x => x.value === user.position)?.label}
               </span>
               <span className="career">{user.career}차</span>
             </div>
-            <div className="user-info-bottom">
-              <span className="label">관심 스택</span>
-              {JSON.parse(user.stack || '[]').map((x: string) => (
-                <span className="stack" key={x}>
-                  {STACKS.find(stack => stack.value === x)?.label}
-                </span>
-              ))}
-            </div>
+            <div className="name">{user.name}</div>
+            <div className="intro">{user.intro || '자기소개가 없습니다.'}</div>
           </div>
         </div>
-        <div className="bottom">{user.intro || '자기소개가 없습니다.'}</div>
-      </Wrppaepr>
-    </Modal>
+        <div className="bottom">
+          <div className="label">관심 스택</div>
+          <div className="stacks">
+            {JSON.parse(user.stack || '[]').map((x: string) => (
+              <span className="stack" key={x}>
+                {STACKS.find(stack => stack.value === x)?.label}
+              </span>
+            ))}
+          </div>
+        </div>
+      </CustomBody>
+    </CustomModal>
   );
 };
