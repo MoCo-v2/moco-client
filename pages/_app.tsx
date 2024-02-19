416bdac9193d0eb096573cf0c7efb54039199fb9
hyeongjin <kimjin5537@daum.net>
2024-02-19 13:42:22 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,6 +33,7 @@ export default function App({Component, pageProps}: AppProps) {
   return (
     <>
       <Head>
+        <title>MOCO</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
