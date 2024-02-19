ee19f332b6e9abdf1ccd485be95e964c4c2d2c19
hyeongjin <kimjin5537@daum.net>
2024-02-19 10:29:44 +0900

    MOCO-100 footer 레이아웃 변경

--- layouts/Footer/index.tsx
+++ layouts/Footer/index.tsx
@@ -3,10 +3,58 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
-  height: 18rem;
-  background: #f9f9f9;
+  height: 20rem;
+  padding: 3rem 6rem;
+  background: #373c42;
+  .top {
+    color: #d5d0d0;
+    font-size: 3.2rem;
+    font-weight: 600;
+    margin-bottom: 1.5rem;
+  }
+  .middle {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    color: #fffbfb;
+    margin-bottom: 1.5rem;
+    .contact {
+      font-size: 1.4rem;
+      font-weight: 400;
+    }
+    .policy {
+      display: flex;
+      gap: 2rem;
+      font-size: 1.4rem;
+      font-weight: 400;
+      div {
+        cursor: pointer;
+      }
+    }
+  }
+  .bottom {
+    color: #d5d0d0;
+    font-size: 1.2rem;
+    text-align: right;
+  }
 `;
 
 export const Footer = () => {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <div className="top">MOCO</div>
+      <div className="middle">
+        <div className="contact">
+          Contact team.moco.offcial@gmail.com
+          <br />
+          Copyright moco. All rights reserved
+        </div>
+        <div className="policy">
+          <div>이용약관</div>
+          <div>개인정보처리방침</div>
+        </div>
+      </div>
+      <div className="bottom">© 2024 MOCO</div>
+    </StyledFooter>
+  );
 };
