8aa25d82708ff8de507cf17487c9c09bac62993e
hyeongjin <kimjin5537@daum.net>
2024-02-19 11:03:40 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,20 +33,6 @@ export default function App({Component, pageProps}: AppProps) {
   return (
     <>
       <Head>
-        <title>MOCO</title>
-        <meta
-          name="description"
-          content="사이드 프로젝트, 멘토링, 스터디 팀빌딩 서비스"
-        />
-        <meta
-          name="keywords"
-          content="스터디, 사이드 프로젝트, 과외, 모각코, 모코, moco"
-        />
-        <meta property="og:title" content="MOCO" />
-        <meta
-          property="og:description"
-          content="사이드 프로젝트, 멘토링, 스터디 팀빌딩 서비스"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,32 @@
+import Head from 'next/head';
+
 import {AppLayout} from '@/layouts/AppLayout';
 
 import {MainBanner, PostList} from '@/containers';
 
 export default function Home() {
   return (
-    <AppLayout>
-      <MainBanner />
-      <PostList />
-    </AppLayout>
+    <>
+      <Head>
+        <title>MOCO</title>
+        <meta
+          name="description"
+          content="사이드 프로젝트, 멘토링, 스터디 팀빌딩 서비스"
+        />
+        <meta
+          name="keywords"
+          content="스터디, 사이드 프로젝트, 과외, 모각코, 모코, moco"
+        />
+        <meta property="og:title" content="MOCO" />
+        <meta
+          property="og:description"
+          content="사이드 프로젝트, 멘토링, 스터디 팀빌딩 서비스"
+        />
+      </Head>
+      <AppLayout>
+        <MainBanner />
+        <PostList />
+      </AppLayout>
+    </>
   );
 }
